ebfdc24074d7ca3fa3809e99bb2fecee7e70eafc
Favaz Muhammed <favasab@gmail.com>
2021-07-18 18:40:32 +0530

    input, print, sum

---
+++ .metadata/.plugins/org.eclipse.core.resources/.history/73/60b8125ec9e7001b1046a687a08f8121
@@ -0,0 +1,12 @@
+
+public class HelloSample {
+	
+	public static void main(String ar[]) {
+		
+		int a =10;
+		
+		System.out.println("Hello World!!!");
+		System.out.println("Value of a is "+a);
+	}
+
+}
--- Hello/src/main/java/HelloSample.java
+++ Hello/src/main/java/HelloSample.java
@@ -1,12 +1,17 @@
+import java.util.Scanner;
 
 public class HelloSample {
 	
 	public static void main(String ar[]) {
 		
-		int a =10;
+		Scanner s=new Scanner(System.in);
+		System.out.println("Enter two values");
 		
-		System.out.println("Hello World!!!");
-		System.out.println("Value of a is "+a);
+		int a = s.nextInt();
+		int b = s.nextInt();
+		
+		int c = a + b;
+		System.out.println("Result:"+c);
 	}
 
 }

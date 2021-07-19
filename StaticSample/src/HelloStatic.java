969104e7850c852414723071050eebeab5a4f235
Favaz Muhammed <favasab@gmail.com>
2021-07-19 15:25:01 +0530

    a static variable have same value in all object, we can assigne for a static variable from any object

--- StaticSample/src/HelloStatic.java
+++ StaticSample/src/HelloStatic.java
@@ -3,12 +3,20 @@ public class HelloStatic {
 	
  
 	int a=10;
+	static int b = 10;
 	public static void main(String ar[]) {
 		
 		HelloStatic h=new HelloStatic();
+		HelloStatic h1=new HelloStatic();
 		System.out.println(h.a);
 		h.hello();
 		hey();
+		
+		h.b= 50;
+		h1.b= 100;
+		
+		System.out.println("h-b: "+h.b);
+		System.out.println("h1-b: "+h1.b);
 	}
 	
 	

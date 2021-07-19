4742cd6eaa5f3e755ab659764436c6645033ad1d
Favaz Muhammed <favasab@gmail.com>
2021-07-19 20:27:23 +0530

    Constructors

---
+++ SampleConstructor/src/Hello.java
@@ -0,0 +1,18 @@
+
+public class Hello {
+	
+	public static void main(String ab[]) {
+		
+		Sample s=new Sample(); //here Sample() is a constructor
+		
+		Hello h=new Hello();
+	}
+	
+//Hello constructor
+	
+	Hello(){
+		
+		System.out.println("This is hello constructor");
+	}
+
+}
---
+++ SampleConstructor/src/Sample.java
@@ -0,0 +1,13 @@
+
+public class Sample {
+	
+	
+	// sample() is a constructor its name is similar same as class name
+	//there will not be a return type similar to a method
+	
+	Sample(){
+		
+		System.out.println("Object creation by favaz_muhammed");
+	}
+
+}

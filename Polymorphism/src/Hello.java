2840462d4975643f31629784d578ca4fcdec8478
Favaz Muhammed <favasab@gmail.com>
2021-07-20 17:53:35 +0530

    Method overloading , Polymorphism

---
+++ Polymorphism/src/Hello.java
@@ -0,0 +1,15 @@
+
+public class Hello {
+	
+	void display(){
+		
+		System.out.println("Nohing to display");
+		
+	}
+	
+	void display(int a) {
+		
+		System.out.println("Value as argument "+a);
+	}
+
+}

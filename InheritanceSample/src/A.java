8ed9e33d995712531dc6186afd4b6d3fbcd188ea
Favaz Muhammed <favasab@gmail.com>
2021-07-20 17:25:33 +0530

    Single inheritance

---
+++ InheritanceSample/src/A.java
@@ -0,0 +1,11 @@
+
+public class A {
+	
+	int a;
+	
+	void displayA() {
+		
+		System.out.println("Welcome to class A");
+	}
+
+}
---
+++ InheritanceSample/src/B.java
@@ -0,0 +1,14 @@
+
+public class B extends A {
+	
+	int num;
+	
+	void displayB() {
+		
+		System.out.println("Welcome to class B");
+	}
+	
+	
+	
+
+}

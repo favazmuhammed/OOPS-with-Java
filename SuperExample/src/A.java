8cde8dcdd5b45ef2c8049c6239ec154a3d1fe633
Favaz Muhammed <favasab@gmail.com>
2021-07-20 18:30:35 +0530

    Constructor with super, example 1

--- SuperExample/src/A.java
+++ SuperExample/src/A.java
@@ -7,5 +7,16 @@ public class A {
 		
 		System.out.println("its base class A");
 	}
+	
+	A(){
+		
+		System.out.println("its A constructor");
+	}
+	
+	A(int a){
+		
+		System.out.println("its argument constructor of A");
+	}
+
 
 }
--- SuperExample/src/B.java
+++ SuperExample/src/B.java
@@ -3,30 +3,28 @@ public class B extends A {
 	
 	int a;
 	
+	B(){
+		
+		System.out.println("its B constructor");
+	}
+	
+	B(int b){
+		
+		System.out.println("its argument constructor of A");
+	}
+	
 	void display() {
 		
 		System.out.println("its sub class B");
 		
-		a = 20;
-		super.a = 30;
-		int c = a + super.a;
-		
-		System.out.println("Sum = "+c);
-	
 		
 	}
 	
-	void displaynew() {
-		
-		super.display();
-	}
 
 	
 	public static void main(String[] args) {
 		
 		B b=new B();
-		b.display();
-		b.displaynew();
 		
 	}
 }

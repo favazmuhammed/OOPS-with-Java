54bd77b878d31a219fe7f9eeac1b47cfe48652cf
Favaz Muhammed <favasab@gmail.com>
2021-07-22 18:15:52 +0530

    String examples

---
+++ StringSample/src/Sample.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Sample {
+	
+public static void main(String[] args) {
+	
+	
+	String name = "Favas";
+	
+	System.out.println(name);
+	
+	System.out.println("Enter the string:");
+	
+	Scanner s=new Scanner(System.in);
+	String str =s.nextLine();
+	
+	System.out.println("string entere is "+str);
+	
+	
+}
+
+}
